f83f7ac6b66c24bfb12891eadb76d7643a035f2d
Varad Chandrawar <s21_chandrawar_varad@mgmcen.ac.in>
2024-11-18 20:00:28 +0530

    Footer changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Box, Typography, Link as MuiLink, IconButton } from '@mui/material';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Footer = () => {
     return (
@@ -19,21 +17,8 @@ const Footer = () => {
             }}
         >
             <Typography variant="body2" align="center" sx={{ mx: 2, fontSize: 14, fontWeight: 500, letterSpacing: 0.5 }}>
-                &copy; {new Date().getFullYear()} GroupiFy. Created by <strong>Pavan Kamthane</strong>
+                &copy; {new Date().getFullYear()} GroupiFy
             </Typography>
-
-            <Box sx={{ display: 'flex', gap: 2, mx: 2 }}>
-                <MuiLink href="https://www.linkedin.com/in/pavankamthane/" target="_blank" color="inherit" underline="hover">
-                    <IconButton size="small" color="inherit" aria-label="LinkedIn">
-                        <LinkedInIcon />
-                    </IconButton>
-                </MuiLink>
-                <MuiLink href="https://github.com/Pavan-Kamthane" target="_blank" color="inherit" underline="hover">
-                    <IconButton size="small" color="inherit" aria-label="GitHub">
-                        <GitHubIcon />
-                    </IconButton>
-                </MuiLink>
-            </Box>
         </Box>
     );
 };
